083e5c0ffc83790c769cede1f00b4fe4442e3e5f
tassosvanis@outlook.com.gr <tassosvanis@outlook.com.gr>
2020-06-07 12:14:13 +0200

    Updates

---
+++ precache-manifest.243aed1ca7ea390df7f75283234ee682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c4e0b565a73571f2406faa4047e8657",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "1866eaf26b336ccd0a76",
+    "url": "/react-monster-rolodex/static/css/main.a3b4eedf.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/react-monster-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1866eaf26b336ccd0a76",
+    "url": "/react-monster-rolodex/static/js/main.8fa6c39b.chunk.js"
+  },
+  {
+    "revision": "c5a071025d6088b67767",
+    "url": "/react-monster-rolodex/static/js/runtime-main.a601bc56.js"
+  }
+]);
